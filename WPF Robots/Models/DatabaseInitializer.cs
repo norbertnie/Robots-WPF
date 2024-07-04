e9befff909724cd627435f3e254a23756a7d3c95
norbertnie <65804691+norbertnie@users.noreply.github.com>
2024-07-04 22:32:28 +0200

    done

--- WPF Robots/Models/DatabaseInitializer.cs
+++ WPF Robots/Models/DatabaseInitializer.cs
@@ -22,7 +22,7 @@ namespace WPF_Robots.Models
             {
                 new Robot { Id = 1, Name = "Chiron", Status = "W trakcie akcji", BatteryLevel = 100, Lokalizacja = "USM 201", Pozycja = "bezpieczna" , Magazyn = "(Dostępny do pracy)" , Stan = "W trakcjie akcji"  },
                 new Robot { Id = 2, Name = "Robot 1", Status = "Dostępny", BatteryLevel = 80, Lokalizacja = "USM 201", Pozycja = "bezpieczna"  , Magazyn = "(Dostępny do pracy)" , Stan = "W trakcjie akcji" },
-                new Robot { Id = 3, Name = "Robot 2", Status = "Niedostępny zajęty", BatteryLevel = 60, Lokalizacja = "USM 201", Pozycja = "bezpieczna"  , Magazyn = "(Zadokowany" , Stan = "W trakcjie akcji" },
+                new Robot { Id = 3, Name = "Robot 2", Status = "Niedostępny zajęty", BatteryLevel = 60, Lokalizacja = "USM 201", Pozycja = "bezpieczna"  , Magazyn = "(Zadokowany)" , Stan = "W trakcjie akcji" },
                 new Robot { Id = 4, Name = "Robot 3", Status = "Niedostępny Potrzebny asystent", BatteryLevel = 60, Lokalizacja = "USM 201", Pozycja = "bezpieczna"  , Magazyn = "(E-stop zwolniony)" , Stan = "W trakcjie akcji" }
             };
 
